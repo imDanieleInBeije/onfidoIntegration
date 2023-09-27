04b00cd1760a159ec05899e912dff0ace879b7ec
imDanieleInBeije <daniele.zappala@beije.it>
2023-09-27 12:59:53 +0200

    commit

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import WebView from 'react-native-webview'
 
 export default function App() {
+  let htmlCode = require('./assets/htmlFile.html')
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <WebView 
+        style={{flex: 1}}
+        source={htmlCode}
+        userAgent='Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1' // without userAgent says "Browser not supported"
+        javaScriptEnabled
+        originWhitelist={['*']} 
+      />
     </View>
   );
 }
@@ -13,8 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    marginVertical: 40,
+    marginHorizontal: 5,
+    borderWidth: 1
+  }
 });
